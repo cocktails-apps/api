96d2f19ea0b877e3b41ab77e22f6e3df4c9bdf39
Dmitry Erlikh <derlih@gmail.com>
2023-11-18 20:30:40 +0100

    API endpoint to return coctails (#6)

--- coctails_api/routes/__init__.py
+++ coctails_api/routes/__init__.py
@@ -1,3 +1,4 @@
+from .coctails import register_coctails_routes
 from .info import register_info_routes
 
-__all__ = ["register_info_routes"]
+__all__ = ["register_coctails_routes", "register_info_routes"]
--- coctails_api/routes/info_test.py
+++ coctails_api/routes/info_test.py
@@ -1,5 +1,18 @@
+import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+
+from .info import HealthResponse, register_info_routes
+
+
+@pytest.fixture
+def app(app: FastAPI) -> FastAPI:
+    register_info_routes(app)
+    return app
 
 
 def test_health(client: TestClient) -> None:
-    pass
+    resp = client.get("/info/health")
+    resp.raise_for_status()
+    health = HealthResponse.model_validate_json(resp.text)
+    assert health.status == "OK"
