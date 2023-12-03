7d06e69a5959836177860768608e8e43e8a07ce8
Dmitry Erlikh <derlih@gmail.com>
2023-12-03 10:12:19 +0100

    fix

--- api/app.py
+++ api/app.py
@@ -1,18 +1,9 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
 from .routes import register_coctails_routes, register_info_routes
-from .storage import get_client, init_collections
+from .storage import get_client
 
 mongo_client = get_client()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_collections(mongo_client)
-
-    yield
 
 
 app = FastAPI()
