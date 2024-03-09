99e1909a4412ae550a01790c73abb79e3cbc84fd
Dmitry Erlikh <derlih@gmail.com>
2024-03-09 15:41:46 +0100

    Vscode lint task (#88)

--- api/log.py
+++ api/log.py
@@ -4,20 +4,24 @@ import logging.config
 def configure_logger(debug: bool) -> None:
     level = "DEBUG" if debug else "INFO"
 
-    logging.config.dictConfig({
-        "disable_existing_loggers": False,
-        "formatters": {
-            "default": {"format": "%(asctime)s %(levelname)s %(message)s (%(name)s)"}
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "default",
-                "level": level,
-            }
-        },
-        "loggers": {
-            "": {"handlers": ["console"], "level": level},
-        },
-        "version": 1,
-    })
+    logging.config.dictConfig(
+        {
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s %(levelname)s %(message)s (%(name)s)"
+                }
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                    "level": level,
+                }
+            },
+            "loggers": {
+                "": {"handlers": ["console"], "level": level},
+            },
+            "version": 1,
+        }
+    )
